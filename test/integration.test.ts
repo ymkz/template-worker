45131c485a902b3c1b91bacbf780601c648794b6
ymkz <ymkz@users.noreply.github.com>
2023-02-17 17:33:20 +0000

    test: add integration test

---
+++ test/integration.test.ts
@@ -0,0 +1,44 @@
+import { afterAll, beforeAll, describe, expect, test } from 'vitest'
+import type { UnstableDevWorker } from 'wrangler'
+import { unstable_dev } from 'wrangler'
+
+/**
+ * @see https://developers.cloudflare.com/workers/wrangler/api/#unstable_dev
+ */
+describe('Worker', () => {
+  let worker: UnstableDevWorker
+
+  beforeAll(async () => {
+    worker = await unstable_dev('src/server/index.ts', {
+      experimental: { disableExperimentalWarning: true },
+    })
+  })
+
+  afterAll(async () => {
+    await worker.stop()
+  })
+
+  test('GET /api/todo should return todos by 200', async () => {
+    const response = await worker.fetch('/api/todo')
+    expect(response.status).toBe(200)
+
+    const responseBody = await response.json()
+    expect(responseBody).toStrictEqual({ todos: [] })
+  })
+
+  test('POST /api/todo/create should create todo by 200', async () => {
+    const response = await worker.fetch('/api/todo/create', {
+      method: 'POST',
+      body: JSON.stringify({ title: 'TODO' }),
+    })
+    expect(response.status).toBe(200)
+
+    const responseBody = await response.json()
+    expect(responseBody).toStrictEqual({
+      result: {
+        id: expect.anything() as string,
+        title: 'TODO',
+      },
+    })
+  })
+})
